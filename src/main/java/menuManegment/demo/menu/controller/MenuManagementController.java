3d93e037fe81f7d98c422f1973298046fa3dbb4b
Amna196 <amnaalshawa96@gmail.com>
2022-01-22 14:09:56 +0300

    clean code and remove comments

--- src/main/java/menuManegment/demo/menu/controller/MenuManagementController.java
+++ src/main/java/menuManegment/demo/menu/controller/MenuManagementController.java
@@ -22,36 +22,4 @@ public class MenuManagementController extends AbstractCRUDController<MenuManagem
         return PATH;
     }
 
-
-//    /**
-//     * Update Status for all
-//     *
-//     * @param status, list of ids
-//     * @return okay, or exception message
-//     */
-//    @PatchMapping("/{status}")
-//    public ResponseEntity<?> updateStatus(@PathVariable("status") String status, @RequestBody List<Integer> ids) {
-//        log.info("<< Calling updateStatus api in AbstractCRUDController.... >>");
-//
-//        // todo: update status for list of integers(id)
-//
-//
-//        System.out.println("models = " + menuManagementService.getAll(ids));
-////        for(Integer id : ids){
-////            System.out.println(", ids = " + id);
-////            //todo: get one upon id
-////            MenuManagementModel model = get(id);
-////            if(status.equals("enable")){
-////                model.setStatus(1);
-////                System.out.println("status = " + status);
-////                System.out.println("model = " + model);
-////            }
-////            model.setStatus(0);
-////            //todo: check status if enable set 1, else set 0 in status of entity => or call update method
-////            service.findAllById(ids);
-////        }
-//        return ResponseEntity.ok().build();
-
-//    }
-
 }
